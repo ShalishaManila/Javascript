7ca54d76eb3d7998a0201db4b817c293a5381d39
ShalishaManila <shalisha.manila@gmail.com>
2025-03-17 20:00:48 +0100

    name,age,color created
    exercise 2 finished

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello,World!");
+
+let firstName= "Shalisha";
+let age=37;
+let color="Red";
+
+console.log(`My name is ${firstName}`);
+console.log(`I am ${age} years old`);
+console.log(`My favorite  color is ${color}`);
