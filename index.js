e81e2b178b89f61298fb237bd26147c938f00e66
ShalishaManila <shalisha.manila@gmail.com>
2025-03-17 20:38:58 +0100

    changes in exercise 2

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ let firstName= "Shalisha";
 let age=37;
 let color="Red";
 
-console.log(`My name is ${firstName}`);
-console.log(`I am ${age} years old`);
-console.log(`My favorite  color is ${color}`);
+console.log(`My name is ${firstName}. I am ${age} years old. My favourite color is ${color}`);
 
 let x=25;
 let y=30;
