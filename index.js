8ba173c8cae6ba35ec445308a07f7cf38d3ad7cc
ShalishaManila <shalisha.manila@gmail.com>
2025-03-17 20:17:39 +0100

    arithmetic operations

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ let color="Red";
 console.log(`My name is ${firstName}`);
 console.log(`I am ${age} years old`);
 console.log(`My favorite  color is ${color}`);
+
+let x=25;
+let y=30;
+
+console.log(`Addition ${x+y}`);
+console.log(`Substraction ${x-y}`);
+console.log(`Multiplication ${x*y}`);
+console.log(`Division ${x/y}`);
